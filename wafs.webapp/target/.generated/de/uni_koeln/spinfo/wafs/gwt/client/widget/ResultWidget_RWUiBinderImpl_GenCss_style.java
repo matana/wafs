c0c13185bfc208511e97dff1a34cedd53dc3e708
Mihail Atanassov <saeko.bjagai@gmail.com>
2013-07-03 23:48:14 +0200

    Set target dir to git ignore

--- wafs.webapp/target/.generated/de/uni_koeln/spinfo/wafs/gwt/client/widget/ResultWidget_RWUiBinderImpl_GenCss_style.java
+++
@@ -1,7 +0,0 @@
-package de.uni_koeln.spinfo.wafs.gwt.client.widget;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface ResultWidget_RWUiBinderImpl_GenCss_style extends CssResource {
-  String important();
-}
